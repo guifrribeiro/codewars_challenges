19994d30b387a3e6bb582229fa46211e6d7a0d85
Guilherme Ferreira <guifrribeiro@gmail.com>
2020-08-28 23:28:41 -0300

    More javascripts challenges.

---
+++ 4kyu/nextBigNumberWithTheSameDigits.js
@@ -0,0 +1,10 @@
+function nextBigger(n) {
+  const sortedDigits = n => ("" + n).split("").sort((a, b) => b - a);
+  const max = +sortedDigits(n).join("");
+
+  for (let i = n + 1; i <= max; i++) {
+    if (max === +sortedDigits(i).join("")) return i;
+  }
+  
+  return -1;
+}
